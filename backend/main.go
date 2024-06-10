539498630994964825b9b829412d128e99926a1e
nsi <n.or.fates@gmail.com>
2024-06-11 07:28:20 +0900

    ✨ PostgreSQLを利用したデータの永続化に対応

--- backend/main.go
+++ backend/main.go
@@ -4,11 +4,17 @@ import (
 	"go-practice-todo/handlers"
 	"go-practice-todo/repositories"
 	"go-practice-todo/services"
+	"log"
 	"net/http"
 )
 
-func main() {
-	repo := repositories.NewTodoRepository()
+func main() { 
+	// repo := repositories.NewTodoRepository()
+	dsn := "host=localhost user=postgres password=postgres dbname=todoapp port=5432 TimeZone=Asia/Tokyo"
+	repo, err := repositories.NewTodoRDBRepository (dsn) 
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
 	service := services.NewTodoService(repo)
 	handler := handlers.NewTodoHandler(service)
 	Routes(handler)
