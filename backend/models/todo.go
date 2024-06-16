0157640fdb816ec3a6bbec03c0bf2d82fcb866e4
nsi <n.or.fates@gmail.com>
2024-06-16 10:27:06 +0900

    🗃️  TodoモデルにCreatedAt/UpdatedAtを追加

--- backend/models/todo.go
+++ backend/models/todo.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -8,5 +10,7 @@ type Todo struct {
 	ID        string `gorm:"primaryKey" json:"id"`
 	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
-	Deleted   gorm.DeletedAt `json:"deleted_at,omitempty"`
+  CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time`json:"updated_at"`
+	Deleted   gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
 }
