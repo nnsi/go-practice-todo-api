b8b1fa70417d98ce778e09d06a4584bf146dbee3
nsi <n.or.fates@gmail.com>
2024-06-21 19:59:27 +0900

    📈 Webasocketの購読/解除時にログを残すようにした

--- backend/infra/websocket.go
+++ backend/infra/websocket.go
@@ -34,14 +34,20 @@ func (n *WebSocketNotifier) RegisterClient(userID string, ws *websocket.Conn) {
 		n.clients[userID] = make(map[*websocket.Conn]bool)
 	}
 	n.clients[userID][ws] = true
+	log.Printf("Registered client: %s, User ID: %s", ws.RemoteAddr(), userID)
 }
 
 func (n *WebSocketNotifier) UnregisterClient(userID string, ws *websocket.Conn) {
 	n.mu.Lock()
 	defer n.mu.Unlock()
-	delete(n.clients[userID], ws)
-	if len(n.clients[userID]) == 0 {
-		delete(n.clients, userID)
+	if _, ok := n.clients[userID][ws]; ok {
+		delete(n.clients[userID], ws)
+		log.Printf("Unregistered client: %s, User ID: %s", ws.RemoteAddr(), userID)
+		if len(n.clients[userID]) == 0 {
+			delete(n.clients, userID)
+		}
+	} else {
+		log.Printf("Attempted to unregister a non-registered client: %s, User ID: %s", ws.RemoteAddr(), userID)
 	}
 }
 
